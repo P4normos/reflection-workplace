111fe8c8834b898617ddee09bf8346ebbc67caa7
Alperen Özcan <ozcanalpe@gmail.com>
2025-10-03 18:17:56 +0300

    jsonwriter

---
+++ src/main/java/org/workplace/jsonwriter/data/Address.java
@@ -0,0 +1,12 @@
+package org.workplace.jsonwriter.data;
+
+public class Address {
+
+    private final String street;
+    private final short apartment;
+
+    public Address(String street, short apartment){
+        this.street = street;
+        this.apartment = apartment;
+    }
+}
